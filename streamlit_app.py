5d269ea8d2712e86d1aa96619b128cdd213e70b7
regaloa <aq3rz10arnu@docomo.ne.jp>
2026-07-09 12:58:09 +0900

    Update and rename streamlit_app.py to app.py

--- streamlit_app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.write("最初の第一歩")
